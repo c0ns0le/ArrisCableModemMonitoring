5b52a19031ea3cc2a2da89cadc7ab944d32cfcc9
Raleigh Littles <raleighlittles@gmail.com>
2018-12-25 23:01:14 -0800

    Added the html for the product information page that is going to need to get parsed eventually. Added empty skeleton methods so I have a better idea of what needs to be done

--- retriever.py
+++ retriever.py
@@ -3,16 +3,37 @@ import bs4
 # read html file in as string
 # pass it to BS parser
 
-with open("html/status_page.html", "r") as status_html:
-    status_html_string = ''.join([line.replace('\n', '') for line in status_html.readlines()])
+
+def extract_status_data(status_page_html=None):
+
+
+    with open("html/status_page.html", "r") as status_html:
+        status_html_string = ''.join([line.replace('\n', '') for line in status_html.readlines()])
+
+
+    beautifulsoup = bs4.BeautifulSoup(status_html_string)
+
+    status_table = beautifulsoup.find_all("table", attrs={'class' : 'simpleTable'})
+
+
+def extract_product_information(product_information_page_html=None):
+    pass
 
 
 
+def construct_status_dict_from_table_html(table_html):
+    """
+    Given the html for all tables on the status page, convert them to a dictionary object
+    """
 
-beautifulsoup = bs4.BeautifulSoup(status_html_string)
+def parse_downstream_channel_info(downstream_table_html):
+    pass
 
-status_table = beautifulsoup.find_all("table", attrs={'class' : 'simpleTable'})
 
-# there's 2 "simple_tables", the one we want is the 2nd one
+def parse_upstream_channel_info(upstream_table_html):
+    pass
 
-print(status_table)
+
+def parse_startup_procedure(startup_table_html):
+    pass
+
